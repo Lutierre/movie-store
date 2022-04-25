4de65378a30677238fabf85bb4c82782cf0190f1
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-04-25 20:22:40 +0200

    Create models

---
+++ MovieStore/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using MovieStore.Models.Enums;
+
+namespace MovieStore.Models;
+
+public class Movie
+{
+    public Guid Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public Genre[] Genres { get; set; }
+    
+    public DateTime ReleaseDate { get; set; }
+}
