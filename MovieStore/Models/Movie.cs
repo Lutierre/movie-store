95ce5514e2f397f0943458fe387cedc6b8679b58
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-04-25 23:19:22 +0200

    Update `Movie` model (add constructor)

--- MovieStore/Models/Movie.cs
+++ MovieStore/Models/Movie.cs
@@ -1,16 +1,27 @@
-﻿using MovieStore.Models.Enums;
+﻿using System.Text.Json.Serialization;
+using MovieStore.Models.Enums;
 
 namespace MovieStore.Models;
 
 public class Movie
 {
-    public Guid Id { get; set; }
-    
+    public Guid Id { get; }
+
     public string Title { get; set; }
-    
-    public string Description { get; set; }
-    
+
+    public string? Description { get; set; }
+
     public Genre[] Genres { get; set; }
-    
+
+    [JsonPropertyName("release_date")]
     public DateTime ReleaseDate { get; set; }
+
+    public Movie(string title, string? description, Genre[] genres, DateTime releaseDate)
+    {
+        Id = Guid.NewGuid();
+        Title = title;
+        Description = description;
+        Genres = genres;
+        ReleaseDate = releaseDate;
+    }
 }
