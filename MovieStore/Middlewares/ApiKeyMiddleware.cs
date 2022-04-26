2c5c0102c4ce34fe2686650849a66c3b699e6947
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-04-26 21:31:42 +0200

    Implement `ApiKeyMiddleware`

---
+++ MovieStore/Middlewares/ApiKeyMiddleware.cs
@@ -0,0 +1,28 @@
+﻿namespace MovieStore.Middlewares;
+
+public class ApiKeyMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _configuration;
+    private const string ApiKeyName = "ApiKey";
+    
+    public ApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        _next = next;
+        _configuration = configuration;
+    }
+    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Request.Headers.TryGetValue(ApiKeyName, out var extractedApiKey);
+        var apiKey = _configuration.GetValue<string>(ApiKeyName);
+ 
+        if (!apiKey.Equals(extractedApiKey))
+        {
+            context.Response.StatusCode = 403;
+            return;
+        }
+ 
+        await _next(context);
+    }
+}
