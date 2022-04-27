fff89c6e85510e10fed8bab98f61c475696f1cb7
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-04-27 21:23:45 +0200

    Little fix `MovieController`

--- MovieStore/Controllers/MovieController.cs
+++ MovieStore/Controllers/MovieController.cs
@@ -6,13 +6,14 @@ using MovieStore.Models;
 namespace MovieStore.Controllers;
 
 [ApiController]
+[ServiceFilter(typeof(TimerFilterAttribute))]
 [Route("Movies")]
 public class MovieController : ControllerBase
 {
     private readonly ILogger<MovieController> _logger;
 
     private readonly IService<Movie> _service;
-
+    
     public MovieController(ILogger<MovieController> logger, IService<Movie> service)
     {
         _logger = logger;
@@ -20,22 +21,17 @@ public class MovieController : ControllerBase
     }
     
     [HttpPost]
-    [ServiceFilter(typeof(TimerFilterAttribute))]
     public Movie Post([FromHeader]string apiKey, [FromBody]Movie movie) => _service.Create(movie);
 
     [HttpGet]
-    [ServiceFilter(typeof(TimerFilterAttribute))]
     public IEnumerable<Movie> Get([FromHeader]string apiKey) => _service.Get();
 
     [HttpGet("{id}")]
-    [ServiceFilter(typeof(TimerFilterAttribute))]
     public Movie? Get([FromHeader]string apiKey, Guid id) => _service.Get(id);
 
     [HttpPatch("{id}")]
-    [ServiceFilter(typeof(TimerFilterAttribute))]
     public Movie? Patch([FromHeader]string apiKey, [FromBody]Movie movie, Guid id) => _service.Update(id, movie);
 
     [HttpDelete("{id}")]
-    [ServiceFilter(typeof(TimerFilterAttribute))]
     public void Delete([FromHeader]string apiKey, Guid id) => _service.Delete(id);
 }
