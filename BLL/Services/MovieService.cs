2b2d001f0d9b1fc27169ad17b3bc04ef9227c03d
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-05-02 23:29:11 +0200

    Update `MovieService`

---
+++ BLL/Services/MovieService.cs
@@ -0,0 +1,25 @@
+﻿using BLL.Abstractions.Interfaces;
+using Core.Models;
+using DAL.Abstractions.Interfaces;
+
+namespace BLL.Services;
+
+public class MovieService : IService<Movie>
+{
+    private readonly IGenericRepository<Movie?> _genericRepository;
+
+    public MovieService(IGenericRepository<Movie?> genericRepository)
+    {
+        _genericRepository = genericRepository;
+    }
+
+    public async Task<List<Movie?>> GetAsync() => await _genericRepository.GetAsync();
+
+    public async Task<Movie?> GetAsync(Guid id) => await _genericRepository.GetAsync(id);
+
+    public async Task<Movie?> CreateAsync(Movie entity) => await _genericRepository.CreateAsync(entity);
+
+    public async Task<Movie?> UpdateAsync(Guid id, Movie? entity) => await _genericRepository.UpdateAsync(id, entity);
+
+    public async Task DeleteAsync(Guid id) => await _genericRepository.DeleteAsync(id);
+}
