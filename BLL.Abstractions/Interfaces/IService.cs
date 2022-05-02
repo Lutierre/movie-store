5d151fa7f15e162f39ae05acdd07b79934f521a7
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-05-02 23:24:26 +0200

    Implement abstractions projects (for BLL & DAL)

---
+++ BLL.Abstractions/Interfaces/IService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+
+namespace BLL.Abstractions.Interfaces;
+
+public interface IService<T> where T : BaseEntity
+{
+    Task<T?> CreateAsync(T entity);
+    
+    Task<List<T>> GetAsync();
+
+    Task<T?> GetAsync(Guid id);
+
+    Task<T?> UpdateAsync(Guid id, T entity);
+
+    Task DeleteAsync(Guid id);
+}
---
+++ DAL.Abstractions/Interfaces/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using Core.Models;
+
+namespace DAL.Abstractions.Interfaces;
+
+public interface IGenericRepository<T> where T: BaseEntity
+{
+    Task<List<T>> GetAsync();
+    
+    Task<T?> GetAsync(Guid id);
+
+    Task<T> CreateAsync(T entity);
+
+    Task<T?> UpdateAsync(Guid id, T? entity);
+    
+    Task DeleteAsync(Guid id);
+}
