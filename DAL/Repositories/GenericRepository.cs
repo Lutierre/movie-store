13019d7dc0210c48cc47b4f5724899952b1fe0de
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-05-02 23:28:45 +0200

    Implement `GenericRepository` and `UnitOfWork`

---
+++ DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using Core.Models;
+using DAL.Abstractions.Interfaces;
+using DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly MovieStoreContext _context;
+    
+    public GenericRepository(MovieStoreContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<T>> GetAsync() => await _context.Set<T>().AsNoTracking().ToListAsync();
+
+    public async Task<T?> GetAsync(Guid id)
+        => await _context.Set<T>().AsNoTracking().Where(entity => entity.Id == id).FirstOrDefaultAsync();
+
+    public async Task<T> CreateAsync(T entity)
+    {
+        await _context.Set<T>().AddAsync(entity);
+        return entity;
+    }
+
+    public async Task<T?> UpdateAsync(Guid id, T? entity)
+    {
+        if (entity == null)
+        {
+            return null;
+        }
+        
+        var existing = await _context.Set<T>().FindAsync(id);
+
+        if (existing == null)
+        {
+            return existing;
+        }
+        
+        _context.Entry(existing).CurrentValues.SetValues(entity);
+
+        return existing;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await Task.Run(()=>
+        {
+            var baseEntity = GetAsync(id).Result;
+
+            if (baseEntity == null)
+            {
+                return null;
+            }
+            
+            return _context.Set<T>().Remove(baseEntity);
+        });
+    }
+}
---
+++ DAL/UnitOfWork.cs
@@ -0,0 +1,63 @@
+﻿using Core.Models;
+using DAL.Abstractions.Interfaces;
+using DAL.Context;
+using DAL.Repositories;
+
+namespace DAL;
+
+public class UnitOfWork : IDisposable
+{
+    private readonly MovieStoreContext _context;
+    private IGenericRepository<Movie>? _movieRepository;
+    private IGenericRepository<Director>? _directorRepository;
+    private IGenericRepository<Comment>? _commentRepository;
+    private IGenericRepository<Genre>? _genreRepository;
+    
+    private bool _disposed = false;
+
+    public IGenericRepository<Movie> MovieRepository => 
+        _movieRepository ??= new GenericRepository<Movie>(_context);
+
+    public IGenericRepository<Director> DirectorRepository => 
+        _directorRepository ??= new GenericRepository<Director>(_context);
+
+    public IGenericRepository<Comment> CommentRepository =>
+        _commentRepository ??= new GenericRepository<Comment>(_context);
+
+    public IGenericRepository<Genre> GenreRepository => 
+        _genreRepository ??= new GenericRepository<Genre>(_context);
+    
+    public UnitOfWork(MovieStoreContext context)
+    {
+        _context = context;
+    }
+    
+    ~UnitOfWork() => Dispose(false);
+    
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+        
+        if (disposing)
+        {
+            _context.Dispose();
+        }
+            
+        _disposed = true;
+    }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    // todo
+    public void Save()
+    {
+        
+    }
+}
