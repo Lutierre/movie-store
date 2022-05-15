8de19545ed16411c069beabc7c9f04f27e20e50c
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-05-15 22:04:53 +0200

    Implement `CommentController`

---
+++ API/Controllers/CommentController.cs
@@ -0,0 +1,25 @@
+﻿using API.ActionFilters;
+using BLL.Abstractions.Interfaces;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[ServiceFilter(typeof(TimerFilterAttribute))]
+[Route("Comments")]
+public class CommentController : ControllerBase
+{
+    private readonly ICommentService _commentService;
+
+    public CommentController(ICommentService commentService)
+    {
+        _commentService = commentService;
+    }
+
+    [HttpPost]
+    public async Task<Comment> Post([FromBody] Comment comment) => await _commentService.CreateAsync(comment);
+
+    [HttpGet("{movieId}")]
+    public async Task<List<Comment>> Get(Guid movieId) => await _commentService.GetByMovieAsync(movieId);
+}
