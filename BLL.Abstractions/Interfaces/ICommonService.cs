9536a2d3cb3cd8debecb100412166db6b9ebd03c
DESKTOP-IHC9G7G\Alice Lutierre <alice.lutierre@gmail.com>
2022-05-22 22:18:49 +0200

    Implement `CommonService` and its abstraction

---
+++ BLL.Abstractions/Interfaces/ICommonService.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using Core.Models;
+using Entities;
+
+namespace BLL.Abstractions.Interfaces;
+
+public interface ICommonService<TModel> where TModel : BaseModel
+{
+    Task<TModel> CreateAsync(TModel model);
+    
+    Task<List<TModel>> GetAsync();
+
+    Task<TModel?> GetAsync(Guid id);
+
+    Task<TModel> GetFilteredAsync(Expression<Func<TModel,bool>> predicate);
+    
+    Task<TModel?> UpdateAsync(Guid id, TModel model);
+
+    Task DeleteAsync(Guid id);
+}
---
+++ BLL/Services/CommonService.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq.Expressions;
+using AutoMapper;
+using BLL.Abstractions.Interfaces;
+using Core.Models;
+using DAL;
+using Entities;
+
+namespace BLL.Services;
+
+public class CommonService<TModel, TEntity> : ICommonService<TModel>
+    where TModel : BaseModel where TEntity : BaseEntity
+{
+    protected readonly UnitOfWork _unitOfWork;
+    protected readonly IMapper _mapper;
+
+    public CommonService(UnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+    
+    public async Task<TModel> CreateAsync(TModel model)
+    {
+        var entity = _mapper.Map<TEntity>(model);
+        entity = await _unitOfWork.GetRepository<TEntity>().CreateAsync(entity);
+        await _unitOfWork.SaveAsync();
+
+        model = _mapper.Map<TModel>(entity);
+        
+        return model;
+    }
+
+    public async Task<List<TModel>> GetAsync()
+    {
+        var entities = await _unitOfWork.GetRepository<TEntity>().GetAsync();
+        var models = entities.Select(entity => _mapper.Map<TModel>(entity)).ToList();
+
+        return models;
+    }
+
+    public async Task<TModel?> GetAsync(Guid id) 
+    {
+        var entity = await _unitOfWork.GetRepository<TEntity>().GetAsync(id);
+        var model = _mapper.Map<TModel>(entity);
+
+        return model;
+    }
+
+    public async Task<TModel> GetFilteredAsync(Expression<Func<TModel, bool>> predicate)
+    {
+        var entity = await _unitOfWork.GetRepository<TEntity>()
+            .GetSingleAsync(model => predicate.Compile()(_mapper.Map<TModel>(model)));
+        var model = _mapper.Map<TModel>(entity);
+        
+        return model;
+    }
+
+    public async Task<TModel?> UpdateAsync(Guid id, TModel model)
+    {
+        var entity = _mapper.Map<TEntity>(model);
+        entity = await _unitOfWork.GetRepository<TEntity>().UpdateAsync(id, entity);
+        await _unitOfWork.SaveAsync();
+        
+        model = _mapper.Map<TModel>(entity);
+        
+        return model;
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        await _unitOfWork.GetRepository<TEntity>().DeleteAsync(id);
+        await _unitOfWork.SaveAsync();
+    }
+}
